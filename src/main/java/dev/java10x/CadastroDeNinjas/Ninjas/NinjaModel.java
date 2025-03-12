0fcd4d1a8a21b162c9500cbe87c2684f0e7ea27c
Willian Bruno <mestrebruno@gmail.com>
2025-03-11 23:53:11 -0300

    Modelagem banco de dados concluido NinjaModel e MissoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 // Entity transforma uma classe em uma entidade do banco dados
 @Entity
@@ -10,9 +13,17 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
+    // @ManyToOne um ninja tem uma única missao.
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreing ou chave estrangeira
+    private MissoesModel missoes;
 
     public NinjaModel() {
     }
